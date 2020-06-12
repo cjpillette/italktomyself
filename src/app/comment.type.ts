ed9faf6e3c002ac0996157078b33ebebecea927e
Carole <cjpillette@hotmail.com>
2020-06-12 15:38:23 +0200

    refactored comment and comment-list components

---
+++ src/app/comment.type.ts
@@ -0,0 +1,6 @@
+export interface Comment {
+  userId: number;
+  title: string;
+  id: number;
+  completed: boolean;
+}
--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-comment',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
+  @Input() comment: Comment;
+  newComment = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addComment() {
+    // show the form field
+  }
+
+  deleteComment() {
+    // hide the form field
   }
 
 }
